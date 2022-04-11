9eb785be09c41dd2fbc1c05e5cec205f5dd06e7e
Matheus Guanaz <matheusguanaz@hotmail.com>
2022-04-11 12:54:55 -0300

    incluindo schema de leitura

--- capgemini-aceleracao-pyspark/scripts/online-retail.py
+++ capgemini-aceleracao-pyspark/scripts/online-retail.py
@@ -1,15 +1,27 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
- 
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
 if __name__ == "__main__":
 	sc = SparkContext()
 	spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Online Retail]"))
 
+	schema_online_retail = StructType([
+		StructField("InvoiceNo", StringType(), True),
+		StructField("StockCode", StringType(), True),
+		StructField("Description", StringType(), True),
+		StructField("Quantity", IntegerType(), True),
+		StructField("InvoiceDate", StringType(), True),
+		StructField("UnitPrice", StringType(), True),
+		StructField("CustomerID", StringType(), True),
+		StructField("Country", StringType(), True)
+	])
+
 	df = (spark.getOrCreate().read
 		          .format("csv")
 		          .option("header", "true")
-		          #.schema(schema_online_retail)
-		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
+		          .schema(schema_online_retail)
+		          .load("/home/spark/capgemini-aceleracao-pyspark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
 	print(df.show())
+
