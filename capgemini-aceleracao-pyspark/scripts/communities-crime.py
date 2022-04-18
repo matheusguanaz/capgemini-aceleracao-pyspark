627344fd6925c474eeaeee6c5bf6f37fa982d745
Matheus Guanaz <matheusguanaz@hotmail.com>
2022-04-18 10:21:08 -0300

    Fazendo qualidade pergunta 1 communities crime

--- capgemini-aceleracao-pyspark/scripts/communities-crime.py
+++ capgemini-aceleracao-pyspark/scripts/communities-crime.py
@@ -3,6 +3,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
  
+
+def pergunta_1_qa(df):
+
+	df = (df.withColumn('PolicOperBudg_qa',
+	 			F.when(
+					(F.col('PolicOperBudg').isNull()) | 
+					(F.col('PolicOperBudg') == '?'),
+				'M')
+				.when(F.col('PolicOperBudg').contains(','), 'F')
+				.when(F.col('PolicOperBudg').rlike('[^0-9.]'), 'A')))
+				
+	df.groupBy('PolicOperBudg_qa').count().show()
+
+	return df
+
+
 if __name__ == "__main__":
 	sc = SparkContext()
 	spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Communities & Crime]"))
@@ -14,3 +30,5 @@ if __name__ == "__main__":
 				  .options(header=True, inferSChema=False)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/capgemini-aceleracao-pyspark/data/communities-crime/communities-crime.csv"))
 	print(df.printSchema())
+
+	df = pergunta_1_qa(df)
